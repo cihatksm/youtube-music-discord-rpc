f666e8b6cedef70de952127a6c7be9df17d2396c
Cihat Kösem <ctk.cihatksm@gmail.com>
2025-06-07 13:54:56 +0300

    Refactor forge.config.js to use ES module syntax, enhance packager configuration with dynamic values from package.json, and update FusesPlugin options for improved functionality

--- forge.config.js
+++ forge.config.js
@@ -1,15 +1,44 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createRequire } from 'node:module';
 
-module.exports = {
+const require = createRequire(import.meta.url);
+const packageJson = require('./package.json');
+
+import { FusesPlugin } from '@electron-forge/plugin-fuses';
+import { FuseV1Options, FuseVersion } from '@electron/fuses';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const name = 'Youtube Music Discord RPC';
+const description = packageJson.description;
+
+export default {
   packagerConfig: {
-    asar: true,
+    name,
+    executableName: packageJson.name,
+    icon: path.resolve(__dirname, 'assets/icon.ico'),
+    asar: {
+      unpack: '*.{node,dll}'
+    },
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        name: packageJson.name,
+        authors: packageJson.author.name,
+        description,
+        iconUrl: path.resolve(__dirname, 'assets/icon.ico'),
+        setupIcon: path.resolve(__dirname, 'assets/icon.ico'),
+        setupExe: packageJson.name + '-' + packageJson.version.replaceAll('.', '_') + '-Setup.exe',
+        setupMsi: packageJson.name + '-' + packageJson.version.replaceAll('.', '_') + '-Setup.msi',
+        shortcuts: {
+          createDesktopShortcut: true
+        }
+      },
     },
     {
       name: '@electron-forge/maker-zip',
@@ -33,7 +62,7 @@ module.exports = {
     // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.RunAsNode]: true,
       [FuseV1Options.EnableCookieEncryption]: true,
       [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
       [FuseV1Options.EnableNodeCliInspectArguments]: false,
